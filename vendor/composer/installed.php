f9ca55d3edc5f111936bb8f07a0f26501bfc1c9b
Alisher007 <alisherx07@gmail.com>
2022-08-15 22:04:38 +0400

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phphleb/hleb',
+        'pretty_version' => 'v1.6.68',
+        'version' => '1.6.68.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phphleb/debugpan' => array(
+            'pretty_version' => 'v1.8',
+            'version' => '1.8.0.0',
+            'reference' => '42eaf9b566699814d7f0a403eceebe8ee61fa104',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phphleb/debugpan',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phphleb/framework' => array(
+            'pretty_version' => 'v1.6.68',
+            'version' => '1.6.68.0',
+            'reference' => 'c8392b056aca937b232fb240ad9c3999f1dfdac9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phphleb/framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phphleb/hleb' => array(
+            'pretty_version' => 'v1.6.68',
+            'version' => '1.6.68.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
